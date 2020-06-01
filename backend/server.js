a5cf466dd3c4d829cb895e209c09da4e183983cc
Christian Castro <alvescastro00cca13@gmail.com>
2020-06-01 00:53:40 -0300

    adicionando nunjucks

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,20 @@ const server  = express()
 
 server.use(express.static("../frontend"))
 
+const nunjucks = require("nunjucks")
+nunjucks.configure("../frontend/views", {
+    express: server,    
+})
+
 server.get("/", function(req, res){
 
-    return res.sendFile(__dirname + "index.html")
+    return res.render("index.html")
+
+})
+
+server.get("ideias", function(req, res){
+
+    return res.render("ideias.html")
 
 })
 
