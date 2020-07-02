2d8fe34c1a5ffed065168e66d68e99e2a448ef75
Christian <alvescastro00cca13@gmail.com>
2020-07-02 17:51:22 -0300

    finished

--- ws/public/script.js
+++ ws/public/script.js
@@ -18,16 +18,32 @@ function onOff(){
         
 }
 
-// function checkFields(event){
+function checkFields(event){
     
-//     const valueToChecked = [
-//         "title",
-//         "image",
-//         "category",
-//         "description",
-//         "link",
-//     ]
-// }
+    const valueToChecked = [
+        "title",
+        "image",
+        "category",
+        "description",
+        "link",
+    ]
+
+    const isEmpty = valueToChecked.find(function(value){
+
+        const checkIfIsString = typeof event.target[value].value === "string"
+        const checkIfIsEmpty = !event.target[value].value.trim()
+
+        if(checkIfIsEmpty && checkIfIsString){
+            return true
+        }
+    })
+
+    if(isEmpty){
+        event.preventDefault()
+        alert("Por favor, preencha todos os campos!")
+    }
+
+}
 
 
 
